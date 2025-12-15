b15feb17b87679925b270b2ed1726be81736a94e
A.Nisanth Noel <nisanthnoel05@gmail.com>
2025-12-15 18:38:34 +0530

    Add files via upload

---
+++ 3 - Quadrant.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Main
+{
+public static void main(String[]args)
+{
+Scanner sc=new Scanner(System.in);
+int x=sc.nextInt();
+int y=sc.nextInt();
+if(x>0&&y>0)
+System.out.println("FirstQuadrant");
+else if(x<0&&y>0)
+System.out.println("SecondQuadrant");
+else if(x<0&&y<0)
+System.out.println("ThirdQuadrant");
+else if(x>0&&y<0)
+System.out.println("FourthQuadrant");
+else
+System.out.println("Axis");
+}
+}
